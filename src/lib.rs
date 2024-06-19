c54548f6bc782f3c6aa6658f2411f27949880010
Shuwa Miura <mana_burn_vo@yahoo.co.jp>
2024-06-19 09:44:25 -0400

    Squashed 'rtdp/' content from commit c14e5eb2
    git-subtree-dir: rtdp
    git-subtree-split: c14e5eb26ed4025f38a9af13456a42a5c2a7fd5b

---
+++ src/lib.rs
@@ -0,0 +1,9 @@
+extern crate mdp;
+
+mod check_solved;
+pub mod rtdp;
+pub mod rtdp_ensure_convergence_wrapper;
+// mod rtdp_policy_evaluation;
+pub mod rtdp_softmax_policy;
+// pub mod rtdp_trait;
+pub mod lrtdp;
